6990bb56e7c15a1308ab1a91902339b12e7a1a27
Pranav Sastry <pranava.sri@gmail.com>
2021-12-05 19:46:22 +0530

    Added tic tac toe.. woof whatta journey i love this

--- tictactoe.py
+++ tictactoe.py
@@ -2,34 +2,45 @@ from copy import deepcopy
 
 # This will be a no nonsense tic tac toe implementation without using OOJ
 
-def game_over(board, num_moves): # Check if someone has won the game or its a draw
+def print_board(board):
+    print()
+    for i in range(len(board)):
+        print(f"{board[i][0]}\t{board[i][1]}\t{board[i][2]}")
+        print()
+    print()
+
+
+def game_over(board, num_moves, print_val=False): # Check if someone has won the game or its a draw
     # Return 0 if x won, 1 if y won, 2 if draw, 3 if nothing
     winner = None
-    if((board[0][0]==board[0][1]==board[0][2]) and (board[0][0] is not None)):
+    if((board[0][0]==board[0][1]==board[0][2]) and (board[0][0]!=None)):
         winner = board[0][0]
-    elif((board[0][0]==board[1][0]==board[2][0]) and (board[0][0] is not None)):
+    elif((board[0][0]==board[1][0]==board[2][0]) and (board[0][0]!=None)):
         winner = board[0][0]
-    elif((board[0][0]==board[1][1]==board[2][2]) and (board[0][0] is not None)):
+    elif((board[0][0]==board[1][1]==board[2][2]) and (board[0][0]!=None)):
         winner = board[0][0]
-    elif((board[0][2]==board[1][2]==board[2][2]) and (board[0][2] is not None)):
+    elif((board[0][2]==board[1][2]==board[2][2]) and (board[0][2]!=None)):
         winner = board[0][2]
-    elif((board[2][0]==board[2][1]==board[2][2]) and (board[2][0] is not None)):
+    elif((board[2][0]==board[2][1]==board[2][2]) and (board[2][0]!=None)):
         winner = board[2][0]
-    elif((board[0][2]==board[1][1]==board[2][0]) and (board[0][2] is not None)):
+    elif((board[0][2]==board[1][1]==board[2][0]) and (board[0][2]!=None)):
         winner = board[0][2]
-    elif((board[0][1]==board[1][1]==board[2][1]) and (board[0][1] is not None)):
+    elif((board[0][1]==board[1][1]==board[2][1]) and (board[0][1]!=None)):
         winner = board[0][1]
-    elif((board[1][0]==board[1][1]==board[1][2]) and (board[1][0] is not None)):
-        winner = board[0][2]
+    elif((board[1][0]==board[1][1]==board[1][2]) and (board[1][0]!=None)):
+        winner = board[1][0]
 
     if(winner is None and num_moves==9):
-        print("It is a draw!")
+        if(print_val):
+            print("\nIt is a draw!")
         return 2
     if(winner=="x"):
-        print("x wins!")
+        if(print_val):
+            print("\nx wins!")
         return 0
-    else:
-        print("y wins!")
+    elif(winner=="o"):
+        if(print_val):
+            print("\no wins!")
         return 1
     return 3
 
@@ -42,8 +53,82 @@ def check_move(board, x, y): # Move can only be allowed if the value in that ind
         return True
     return False
 
-def search_move(board, computer_pawn, num_moves): # Computer to search for an optimal move
-    pass
+def all_board_possibilities(board, pawn, num_moves):
+    # Check for all possibilities on the board if all None in the board are replaced by "pawn" in the board everytime
+    # and everytime a new board appears, giving all possibilities.
+    # If x is pawn and x is winning, then that move will be returned, so on that move o(computer)
+    # can play o in that coordinates and hence prevent it from winning. Same appears if pawn is o
+    possibilities = []
+    moves = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if(board[i][j]==None):
+                board_x = deepcopy(board)
+                board_x[i][j] = pawn
+                possibilities.append(board_x)
+                moves.append((i,j))
+    for i,p in enumerate(possibilities):
+        go = game_over(p,num_moves)
+        if((pawn=="x" and go==0) or (pawn=="o" and go==1)):
+            return moves[i]
+    return None
+
+def make_consecutives(board, computer_pawn):
+    # If no one, x or o is winning, then try to make consecutive pawns be it left,right,up,down or diagonals.
+    """
+        Example:
+            before 
+                o - -
+                x - -
+                - - -
+            
+            after
+                o o -
+                x - -
+                - - -
+
+                or
+
+                o - -
+                x o -
+                - - -
+    """
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if(board[i][j]==computer_pawn):
+                if(i-1>=0 and board[i-1][j]==None):
+                    return (i-1,j)
+                if(i+1<len(board) and board[i+1][j]==None):
+                    return (i+1,j)
+                if(j-1>=0 and board[i][j-1]==None):
+                    return (i,j-1)
+                if(j+1<len(board) and board[i][j+1]==None):
+                    return (i,j+1)
+                if((i-1>=0 and j-1>=0) and board[i-1][j-1]==None):
+                    return (i-1,j-1)
+                if((i-1>=0 and j+1<len(board)) and board[i-1][j+1]==None):
+                    return (i-1,j+1)
+                if((i+1<len(board) and j-1>=0) and board[i+1][j-1]==None):
+                    return (i+1,j-1)
+                if((i+1<len(board) and j+1<len(board)) and board[i+1][j+1]==None):
+                    return (i+1,j+1)
+    return None
+
+def search_move(board, computer_pawn, user_pawn, num_moves): # Computer to search for an optimal move
+    does_user_win = all_board_possibilities(board,user_pawn,num_moves)
+    if(does_user_win!=None):
+        return make_move(board,does_user_win[0],does_user_win[1],computer_pawn)
+    does_computer_win = all_board_possibilities(board,computer_pawn,num_moves)
+    if(does_computer_win!=None):
+        return make_move(board,does_computer_win[0],does_computer_win[1],computer_pawn)
+    consecutives = make_consecutives(board,computer_pawn)
+    if(consecutives!=None):
+        return make_move(board,consecutives[0],consecutives[1],computer_pawn)
+    # if none of them win and consecutives also aren't possible, then make the move anywhere it is None  
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if(board[i][j]==None):
+                return make_move(board,i,j,computer_pawn)
 
 def get_move(board): # Get the move from the user FORMAT-> x y, where they are indices of the board
     while(True):
@@ -53,11 +138,11 @@ def get_move(board): # Get the move from the user FORMAT-> x y, where they are i
             x = int(move_str_split[0])
             y = int(move_str_split[1])
         except:
-            print("Invalid move format! Try again.")
+            print("Invalid move format! Try again.\n")
             continue
         if(check_move(board,x,y)):
             return x,y
-        print("Invalid move! Try again.")
+        print("Invalid move! Try again.\n")
 
 def choose_pawn():
     while(True):
@@ -68,7 +153,7 @@ def choose_pawn():
         if(user_pawn=="o"):
             computer_pawn = "x"
             break
-        print("Invalid input! Try again!")
+        print("Invalid input! Try again!\n")
     return user_pawn, computer_pawn
 
 def make_move(board, x, y, player): # Make the move on the board
@@ -84,20 +169,22 @@ def play(board): # Main function which is the communication bw the board and use
         players = [computer_pawn, user_pawn]
     else:
         players = [user_pawn, computer_pawn]
-    while(game_over(board,num_moves)==3):
+    while(game_over(board,num_moves,True)==3):
+        print_board(board)
         curr_player = players[num_moves%len(players)]
         if(curr_player==computer_pawn):
-            x,y = search_move(board, computer_pawn, num_moves)
+            board = search_move(board, computer_pawn, user_pawn, num_moves)
         else:
             x,y = get_move(board)
-        board = make_move(board,x,y,curr_player)
+            board = make_move(board,x,y,curr_player)
         num_moves+=1
+    return board
 
 if __name__=='__main__':
     board = [[],[],[]]
     for row in range(len(board)): # Create empty board and initialize everything with None
         for _ in range(len(board)):
             board[row].append(None)
-    play(board)
+    board = play(board)
 
 
